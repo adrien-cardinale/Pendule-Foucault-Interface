58ec62d3e0a5b07bcfe7007b1dcda8bac325698c
Adrien Cardinale <adrien.cardinale@heig-vd.ch>
2023-07-04 11:46:18 +0200

    add room

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, emit
 import csv
 import threading
 import time
@@ -7,7 +7,6 @@ import time
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
 socketio = SocketIO(app)
-
 
 def get_data():
     print("get_data")
@@ -23,23 +22,23 @@ def get_data():
             timestamp = lines[i].split(',')[2].split(".")[0]
             data.append({'x': x, 'y': y, 'timestamp': timestamp})
     print("data ready")
-    
 
 @socketio.on('connect')
 def handle_connect():
-    socketio.emit('metaData', {'dataLength': len(data)})
-
+    room_id = request.sid  # Utilisez l'ID de session comme identifiant de la chambre
+    join_room(room_id)
+    emit('metaData', {'dataLength': len(data)}, room=room_id)
 
 @socketio.on('get_data')
 def handle_get_data(dataIndex):
-    socketio.emit('data', data[int(dataIndex["time"]):int(dataIndex["time"]) + int(dataIndex["points"])])
+    room_id = request.sid
+    emit('data', data[int(dataIndex["time"]):int(dataIndex["time"]) + int(dataIndex["points"])], room=room_id)
 
 @socketio.on('get_labels')
 def handle_get_labels():
+    room_id = request.sid
     labels = {"minTime": data[0]['timestamp'], "maxTime": data[-1]['timestamp']}
-    socketio.emit('labels', labels)
-
-
+    emit('labels', labels, room=room_id)
 
 @app.route('/')
 def index():
@@ -48,7 +47,6 @@ def index():
 @app.route('/mesures')
 def mesures():
     return render_template('mesures.html')
-
 
 if __name__ == '__main__':
     get_data()
