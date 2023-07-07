e7809bafaa951508c66132501cf4b83758beb353
Adrien Cardinale <adrien.cardinale@heig-vd.ch>
2023-07-07 15:30:47 +0200

    moteur

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ import cv2
 from datetime import datetime
 import os
 import eventlet
+import numpy as np
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
 socketio = SocketIO(app)
+
+xCenter = 1203
+yCenter = 1357
+path = os.path.expanduser('~')+ "/data/"
+files = os.listdir(path)
 
 def get_data(date):
     path = os.path.expanduser('~')+ "/data/"
@@ -46,31 +52,52 @@ def generate_frames():
     # Libérer les ressources après la fin de la diffusion
     cap.release()
 
-def read_data():
-    path = os.path.expanduser('~')+ "/data/"
-    files = os.listdir(path)
+def get_date():
     global dates
     dates = []
     for file in files:
-        dates.append(file.split("-", 1)[1])
+        if file.startswith("pFposition-"):
+            dates.append(file.split("-", 1)[1])
     dates.sort()
-    get_data(dates[-1])
-    while True:
-        with open(path + 'pFposition-' + dates[-1], 'r') as file:
-            lines = file.readlines()
-            last_line = lines[-1]
-            t = last_line[0:9]
-            x = last_line[9:13]
-            y = last_line[13:17]
-            socketio.emit('data2', [{'x': x, 'y': y, 'timestamp': t}])
-        time.sleep(0.05)
 
+def read_data():
+    get_data(dates[-1])
+    amplitude = 0
+    while True:
+        x = []
+        y = []
+        for i in range(0, 400):
+            with open(path + 'pFposition-' + dates[-1], 'r') as file:
+                lines = file.readlines()
+                last_line = lines[-1]
+                _t = last_line[0:9]
+                _x = last_line[9:13]
+                _y = last_line[13:17]
+                socketio.emit('data2', [{'x': _x, 'y': _y, 'timestamp': _t}])
+                x.append(int(_x))
+                y.append(int(_y))
+            time.sleep(0.05)
+            socketio.emit('amplitude', {'amplitude': amplitude})
+        amplitude = int(np.sqrt((np.max(x)-xCenter) ** 2 + (np.max(y)-yCenter) ** 2))
+
+def read_driver_data():
+    data3 = []
+    with open(path + 'pFregulateur-' + dates[-1], 'r') as file:
+        lines = file.readlines()
+        for i in range(0, 1000):
+            t = lines[i].split(",")[0]
+            y = lines[i].split(",")[1]
+            data3.append({'t': t, 'y': y})
+    socketio.emit('data3', data3)
+                
+        
 
 @socketio.on('connect')
 def handle_connect():
     room_id = request.sid  # Utilisez l'ID de session comme identifiant de la chambre
     join_room(room_id)
     emit('metaData', {'dataLength': len(data)}, room=room_id)
+    read_driver_data()
 
 @socketio.on('get_data')
 def handle_get_data(dataIndex):
@@ -95,7 +122,14 @@ def index():
 
 @app.route('/mesures')
 def mesures():
+    get_date()
     return render_template('mesures.html', dates=dates)
+
+@app.route('/moteur')
+def moteur():
+    read_driver_data()
+    return render_template('moteur.html')
+
 
 @app.route('/camera')
 def camera():
@@ -107,6 +141,7 @@ def video_feed():
 
 
 if __name__ == '__main__':
+    get_date()
     eventlet.monkey_patch()
     eventlet.spawn(read_data)
     # thread = threading.Thread(target=read_data)
